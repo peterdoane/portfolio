39a7870c5753ec962d962edb33a7f8ef87d55672
Peter Doane <peterdoane@gmail.com>
2017-08-25 11:08:13 -0700

    overlays

--- src/latestWork/index.jsx
+++ src/latestWork/index.jsx
@@ -2,39 +2,51 @@ import React from 'react';
 import './latestWork.css';
 
 const LatestWork = () => {
-  return (
-    <section className="container latest">
-      <div className="innerColumn">
-        <header>
-          <h2>Latest Work</h2>
-          <p>
-            Big, small, online, offline, local or international.Size doesn 't matter. We work on diverse projects for top brands as well as for cool startups. Check out some of our favorites.
-          </p>
-        </header>
-        <ul className="projectList">
-          <li className="projectsSection">
-          <a>
-            <figure>
-              <img src="images/360modernlatest.png"/>
-            </figure>
-            <h3>360modern</h3>
-            <p>React / Real Esate </p>
-          </a>
-          </li>
-          <li className="projectsSection">
-            <figure>
-              <img src="images/attractions1.png"/>
-            </figure>
-            <h3>Attractions</h3>
-            <p>React-Native</p>
-          </li>
-        </ul>
-        <footer>
-          <a className="button">View All Work</a>
-        </footer>
-      </div>
-    </section>
-  );
+return (
+<section className="container latest">
+<div className="innerColumn">
+<header>
+<h2>Latest Work</h2>
+<p>
+Big, small, online, offline, local or international.Size doesn 't matter. We work on diverse projects for top brands as well as for cool startups. Check out some of our favorites.
+</p>
+</header>
+<ul className="projectList">
+<li className="projectsSection">
+<div className="content">
+<a>
+<figure>
+<div className="content-overlay"></div>
+<img className="content-image" src="images/360modernlatest.png" />
+<div className="content-details fadeIn">
+<h3 className="content-title">360modern</h3>
+<p className="content-text">React / Real Esate </p>
+</div>
+</figure>
+</a>
+</div>
+</li>
+<li className="projectsSection">
+<div className="content">
+<a>
+<figure>
+<div className="content-overlay"></div>
+<img className="content-image" src="images/attractions1.png" />
+<div className="content-details fadeIn">
+<h3 className="content-title">Attractions</h3>
+<p className="content-text">React-Native</p>
+</div>
+</figure>
+</a>
+</div>
+</li>
+</ul>
+<footer>
+<a className="button">View All Work</a>
+</footer>
+</div>
+</section>
+);
 }
 
 export default LatestWork;
@@ -48,6 +60,6 @@ export default LatestWork;
 
 
 {/* <img src="images/AppIcon.Attractions-logo.png" alt="360modernport" title="360modernport" />
-<img src="images/CausticAcrostic.png"  />
-<img src="images/CausticAcrostic.png"  />
-<img src="images/CausticAcrostic.png"  /> */}
+<img src="images/CausticAcrostic.png" />
+<img src="images/CausticAcrostic.png" />
+<img src="images/CausticAcrostic.png" /> */}
